07b20bd1c76afae5a1afcf69c11827447bad9ccc
thetran-zalora <minhthefortest.007@gmail.com>
2025-08-03 10:25:06 +0700

    feat: start assignment solution

---
+++ src/problem3/solution.ts
@@ -0,0 +1,11 @@
+function sum_to_n_a(n: number): number {
+    // your code here
+}
+
+function sum_to_n_b(n: number): number {
+    // your code here
+}
+
+function sum_to_n_c(n: number): number {
+    // your code here
+}
